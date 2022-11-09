4cc0bc9a37b8d750f31631cbf5c8103179860a8e
Okiki Olanrewaju <109441216+iKiks@users.noreply.github.com>
2022-11-09 15:10:00 +0100

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void){
+int main(void)
+{
+    \\ greet user
+    string name = get_string("What's your name? ");
+    printf("hello, %s\n", name);
+}
